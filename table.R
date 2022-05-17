e98ce1594ead79db14246020a293271912b43694
aliyajali <aliyaa3@uw.edu>
2022-05-16 19:15:54 -0700

    testing again

---
+++ table.R
@@ -0,0 +1,18 @@
+# Aggregate table script
+# Aliya Ali
+# 05/16/22
+# Group members: Kaia Truong, Aliya Ali, Randy Ros, Vince Qian
+
+#load the datasets and package.
+library(tidyverse)
+covid_vaccination <- read.csv("/Users/aliyaali/final-projects-randyr02/vaccinations.csv")
+covid_data <- read.csv("/Users/aliyaali/final-projects-randyr02/owid-covid-data.csv")
+
+#new dataset for the covid data that includes only needed columns
+covid_dataset <- covid_data[c("date", "total_cases", "new_cases", "total_deaths", "new_deaths", "total_vaccinations")]
+
+#new dataset for the vaccination data that includes only needed columns
+vaccination_dataset <- covid_vaccination[c("date", "total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "total_boosters")]
+
+#combined dataset of both dataframes
+covid <- left_join(covid_dataset, vaccination_dataset, by = "date")
