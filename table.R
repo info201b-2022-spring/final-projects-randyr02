ed71ea73edbebd6781affc55468c2f4af852e439
aliyajali <aliyaa3@uw.edu>
2022-05-17 18:52:12 -0700

    updated

--- table.R
+++ table.R
@@ -9,13 +9,14 @@ covid_vaccination <- read.csv("/Users/aliyaali/final-projects-randyr02/vaccinati
 covid_data <- read.csv("/Users/aliyaali/final-projects-randyr02/owid-covid-data.csv")
 
 #new dataset for the covid data that includes only needed columns
-covid_dataset <- covid_data[c("date", "total_cases", "new_cases", "total_deaths", "new_deaths", "total_vaccinations")]
+covid_dataset <- covid_data[c("location" ,"date","total_cases", "new_cases", "total_deaths", "new_deaths", "total_vaccinations")]
 
 #new dataset for the vaccination data that includes only needed columns
-vaccination_dataset <- covid_vaccination[c("date", "total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "total_boosters")]
+vaccination_dataset <- covid_vaccination[c("location", "date", "total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "total_boosters")]
 
 #combined dataset of both dataframes
 covid <- left_join(covid_dataset, vaccination_dataset, by = "date")
 
+#new data set that is grouped by date and includes the average number of cases and deaths
 grouped <- group_by(covid, date)
 avg_values <- summarise(grouped, avg_cases = mean(total_cases, na.rm = TRUE), avg_deaths = mean(total_deaths, na.rm = TRUE))
