08d61593d9ba16af8d0b09ad4064612d050a20e3
Minh Anh Truong <tmanh1312@gmail.com>
2022-05-17 17:18:14 -0700

    submit

--- summary.R
+++ summary.R
@@ -21,17 +21,25 @@ covid_data <- read.csv("owid-covid-data.csv")
 # A function that takes in `covid_vaccination` dataset and returns a list 
 # of info about it:
 summary_vaccinations_info <- list() 
+
 summary_vaccinations_info$num_observations <- nrow(covid_vaccination)
+
 summary_vaccinations_info$num_features <- ncol(covid_vaccination)
+
 summary_vaccinations_info$world_total_vaccinations <- covid_vaccination %>%
   filter(total_vaccinations == max(total_vaccinations, na.rm = T)) %>%
   select(location, total_vaccinations) 
+
 summary_vaccinations_info$number_of_people_fully_vaccinated <- covid_vaccination %>%
   filter(people_fully_vaccinated == max(people_fully_vaccinated, na.rm = T)) %>%
   select(location, people_fully_vaccinated) 
-sub_vaccination_df <- covid_vaccination[!(covid_vaccination$location == "World" | covid_vaccination$location =="High income" | covid_vaccination$location =="Upper middle income"), ]
+
+sub_vaccination_df <- covid_vaccination[!(covid_vaccination$location == "World" | covid_vaccination$location =="High income" | covid_vaccination$location =="Upper middle income"), ] 
+
+summary_vaccinations_info$continent_highest_total_vaccinations <- sub_vaccination_df %>%
   filter(total_vaccinations == max(total_vaccinations, na.rm = T)) %>%
   select(location, total_vaccinations) 
+
 summary_vaccinations_info$continent_most_people_vaccinated <- sub_vaccination_df %>%
   filter(people_fully_vaccinated == max(people_fully_vaccinated, na.rm = T)) %>%
   select(location, people_fully_vaccinated) 
@@ -40,19 +48,27 @@ summary_vaccinations_info$continent_most_people_vaccinated <- sub_vaccination_df
 # A function that takes in `covid_data` dataset and returns a list 
 # of info about it:
 summary_data_info <- list() 
+
 summary_data_info$num_observations <- nrow(covid_data)
+
 summary_data_info$num_features <- ncol(covid_data)
+
 summary_data_info$world_total_deaths <- covid_data %>%
   filter(total_deaths == max(total_deaths, na.rm = T)) %>%
   select(location, total_deaths) 
+
 summary_data_info$world_total_cases <- covid_data %>%
   filter(total_cases == max(total_cases, na.rm = T)) %>%
   select(location, total_cases) 
-sub_data_df <- covid_data[!(covid_data$location == "World" | covid_data$location =="High income" | covid_data$location =="Upper middle income"), ]
+
+sub_data_df <- covid_data[!(covid_data$location == "World" | covid_data$location =="High income" | covid_data$location =="Upper middle income"), ] 
+
 summary_data_info$location_max_deaths <- sub_data_df %>%
   filter(total_deaths == max(total_deaths, na.rm = T)) %>%
   select(location, total_deaths) 
+
 summary_data_info$location_max_cases <- sub_data_df %>%
   filter(total_cases == max(total_cases, na.rm = T)) %>%
   select(location, total_cases) 
+
 
