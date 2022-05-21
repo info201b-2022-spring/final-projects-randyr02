a9ea2639c4e0b8ce3ff07ca352e039f9140b88db
Minh Anh Truong <tmanh1312@gmail.com>
2022-05-20 21:06:33 -0700

    add rmd_report

--- table.R
+++ table.R
@@ -13,3 +13,19 @@ covid_dataset <- covid_data[c("iso_code", "location" ,"date","total_cases", "new
 #new data set that is grouped by date and includes the average number of cases and deaths
 grouped <- group_by(covid_dataset, date)
 avg_values <- summarise(grouped, avg_cases = mean(total_cases, na.rm = TRUE), avg_deaths = mean(total_deaths, na.rm = TRUE))
+
+#value of every 4 months to report in .rmd file 
+# at different locations
+library(lubridate)
+sum_4months_report <- covid_dataset %>%
+  mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
+  arrange(date) %>%
+  group_by(location, start_date = floor_date(date, "4 months")) %>%
+  summarise(value = na.omit(sum(total_cases)))
+
+# `rmd.report` reports the total number of cases around the 
+# world every 4 months with the start date in the `start_date` 
+# column
+rmd_report <- sum_4months_report %>%
+  group_by(start_date) %>%
+  summarise(total_cases_in_4_months= sum(value))
