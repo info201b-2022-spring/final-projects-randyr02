319687f05207ab9a43cb81ef691b4695436f35c1
Minh Anh Truong <tmanh1312@gmail.com>
2022-05-20 21:41:07 -0700

    fix rmd_repoprt

--- table.R
+++ table.R
@@ -5,27 +5,29 @@
 
 #load the datasets and package.
 library(tidyverse)
+library(stringr)
 covid_data <- read.csv("owid-covid-data.csv")
 
 #new dataset for the covid data that includes only needed columns
 covid_dataset <- covid_data[c("iso_code", "location" ,"date","total_cases", "new_cases", "total_deaths", "new_deaths", "total_vaccinations")]
+sub_data_df <- covid_data[!(covid_data$location == "World" | covid_data$location == str_detect("income"))]
 
 #new data set that is grouped by date and includes the average number of cases and deaths
-grouped <- group_by(covid_dataset, date)
+grouped <- group_by(sub_data_df, date)
 avg_values <- summarise(grouped, avg_cases = mean(total_cases, na.rm = TRUE), avg_deaths = mean(total_deaths, na.rm = TRUE))
 
 #value of every 4 months to report in .rmd file 
 # at different locations
 library(lubridate)
-sum_4months_report <- covid_dataset %>%
+sum_4months_report <- sub_data_df %>%
   mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
   arrange(date) %>%
   group_by(location, start_date = floor_date(date, "4 months")) %>%
-  summarise(value = na.omit(sum(total_cases)))
+  summarise(value = na.omit(max(total_cases)))
 
 # `rmd.report` reports the total number of cases around the 
 # world every 4 months with the start date in the `start_date` 
 # column
 rmd_report <- sum_4months_report %>%
   group_by(start_date) %>%
-  summarise(total_cases_in_4_months= sum(value))
+  summarise(total_cases_in_4_months= max(value))
