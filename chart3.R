b4bd864bcc3a29858bc12abdc2ef1674b83bc896
VinceQian <qqian2@uw.edu>
2022-05-22 23:38:14 -0700

    annotations of charts

--- chart3.R
+++ chart3.R
@@ -1,8 +1,19 @@
+# Chart 3 
+# Author: Vince Qian
+# Exploratory Data Analysis INFO 201
+
+## This chart attempts to display an interactive bar chart to show
+## the total deaths over time.
+
+# Load libraries
 library("dplyr")
 library("ggplot2")
 library("plotly")
 
+# Create the bar chart function
 get_bar <- function(df, area) {
+  
+  # Calculate the summaries of each month
   trend <- df %>%
     filter(iso_code == area) %>%
     mutate(month = strftime(date, format = "%Y-%m")) %>%
@@ -10,11 +21,13 @@ get_bar <- function(df, area) {
     summarise(monthly_total_deaths = max(total_deaths),
               monthly_new_deaths = mean(new_deaths_per_million))
   
+  # Get the latest data of a location
   country <- df %>%
     filter(iso_code == area) %>%
     filter(date == max(date)) %>%
     pull(location)
   
+  # Create the bar chart
   bar <- ggplot(data = trend) +
     geom_col(mapping = aes(x = month, y = monthly_total_deaths,
                            fill = monthly_new_deaths))+
@@ -25,8 +38,9 @@ get_bar <- function(df, area) {
     theme(axis.text.x = element_text(angle = 90)) +
     scale_y_continuous(labels = function(x) format(x, scientific = FALSE)) +
     scale_fill_gradient(low = "white", high = "black")
-
-    ggplotly(bar)
+  
+  # Make it interactive
+  ggplotly(bar)
 }
 
 get_bar(covid_data, "OWID_WRL")
