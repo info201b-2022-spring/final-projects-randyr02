4623b8e06377bbb888393303ffb693de3e9fca53
Minh Anh Truong <tmanh1312@gmail.com>
2022-06-03 15:18:20 -0700

    new data

--- chart2.R
+++ chart2.R
@@ -11,32 +11,35 @@ library("dplyr")
 library("ggplot2")
 library("plotly")
 
+covid_data <- read.csv("owid-covid-data.csv")
 # Create the scatter plot function
 get_scatter <- function(df) {
-  
+
   # Remove NAs and non-location observations, and get the latest data
   latest_data <- df %>%
     filter(!is.na(total_vaccinations_per_hundred)) %>%
     filter(!is.na(total_deaths_per_million)) %>%
     filter(continent == "Africa" | continent == "Asia" |
-             continent == "Europe" | continent == "North America" |
-             continent == "Oceania" | continent == "South America") %>%
+           continent == "Europe" | continent == "North America" |
+           continent == "Oceania" | continent == "South America") %>%
     group_by(iso_code) %>%
     filter(date == max(date))
   
   # Create the scatter plot
   scatter <- ggplot(data = latest_data) +
-    geom_point(mapping = aes(x = total_vaccinations_per_hundred,
-                             y = total_deaths_per_million,
+    geom_point(mapping = aes(x = new_vaccinations_smoothed_per_million,
+                             y = new_deaths_smoothed_per_million,
                              color = continent,
                              text = paste("country:", location, "<br>", 
                                           "updated:", date))
     ) +
     labs(
-      x = "Total Vaccinations per hundred",
-      y = "Total Deaths per million",
+      x = "New Vaccinations per million",
+      y = "New Deaths per million",
       title = "Total Deaths vs Total Vaccinations")
   
   # Make it interactive
   ggplotly(scatter)
 }
+
+get_scatter(covid_data)
